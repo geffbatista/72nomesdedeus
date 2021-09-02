ea2cef89743b221f96cf84d94a11a1340469134c
Gefferon Batista <geffbatista@gmail.com>
2021-09-02 19:14:58 -0300

    Fix types

--- Components/NavigationByPosition/types.ts
+++ Components/NavigationByPosition/types.ts
@@ -1,3 +1,6 @@
+import { Dispatch, SetStateAction } from "react";
+import { NomesDeDeusType } from "../../types";
+
 export interface NavigationByPositionProps {
   SetentaEDoisNomesDeDeus: NomesDeDeusType;
   setShowPanel: Dispatch<SetStateAction<boolean>>;
