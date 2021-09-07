37ae78675ecf05574a0f23afd1a2db0a662acb58
Gefferon Batista <geffbatista@gmail.com>
2021-09-07 16:03:20 -0300

    Time to lunch

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
-    dest: "public",
+    disable: !isProduction,
+    dest: "pwa",
+    sw: "/pwa",
+    runtimeCaching,
   },
 });
